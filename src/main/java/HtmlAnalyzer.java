1f8df6beef2f5695c5619111d46dea4400172f5f
Andreas Kogler <andy.kogler.junior@gmail.com>
2021-03-16 16:58:29 +0100

    Init project structure

---
+++ src/main/java/HtmlAnalyzer.java
@@ -0,0 +1,5 @@
+/*
+    Provides statistics over the parsed HTML object of the HtmlGrabber class.
+ */
+public class HtmlAnalyzer {
+}
---
+++ src/main/java/HtmlGrabber.java
@@ -0,0 +1,5 @@
+/*
+    Responsible for retrieving & parsing HTML from a given page.
+ */
+public class HtmlGrabber {
+}
